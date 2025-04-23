955235e1c5b4e8aec1c60a9605d0eb23cbcd5c52
yanjinlkham9 <bilgunn93@gmail.com>
2025-04-23 14:51:43 +0900

    server setting

---
+++ server/hibye_server/src/main/java/com/hibye_server/hibye_server/HibyeServerApplication.java
@@ -0,0 +1,13 @@
+package com.hibye_server.hibye_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibyeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibyeServerApplication.class, args);
+	}
+
+}
---
+++ server/hibye_server/src/test/java/com/hibye_server/hibye_server/HibyeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibye_server.hibye_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibyeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
